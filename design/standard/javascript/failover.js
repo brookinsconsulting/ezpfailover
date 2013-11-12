5852cca4575e7055edf76648257c35f3f4169891
Brookins Consulting <info@brookinsconsulting.com>
2013-11-12 11:11:14 -0600

    Updated: Added anonymous function wrapper

--- design/standard/javascript/failover.js
+++ design/standard/javascript/failover.js
@@ -1,16 +1,16 @@
+$(function(){
+  // alert('failover.js loaded');
 
-// alert('failover.js loaded');
-
-var jqxhr = $.ajax({
-    type: "GET",
-    url: "/failover/test"
-})
-.done(function( data ) {
-    // alert( "request complete" );
-    // alert( data );
-    if ( data == '1' )
-    {
-        $('body').first().append('<div class="failover-alert">Warning: You are using a failover mirror of the site! Your changes will be lost when service is restored!</div>');
-    }
+  var jqxhr = $.ajax({
+      type: "GET",
+      url: "/failover/test"
+  })
+  .done(function( data ) {
+      // alert( "request complete" );
+      // alert( data );
+      if ( data == '1' )
+      {
+          $('body').first().append('<div class="failover-alert">Warning: You are using a failover mirror of the site! Your changes will be lost when service is restored!</div>');
+      }
+  });
 });
-
