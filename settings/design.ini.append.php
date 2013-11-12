9b15c388c175049f4441da2ff8a8e57f65ac2ccc
Brookins Consulting <info@brookinsconsulting.com>
2013-11-12 11:11:14 -0600

    Updated: Added support for newer editions of eZ Publish in design settings offered

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -5,8 +5,12 @@ DesignExtensions[]=ezpfailover
 
 [StylesheetSettings]
 CSSFileList[]=failover.css
+FrontendCSSFileList[]=failover.css
+BackendCSSFileList[]=failover.css
 
 [JavaScriptSettings]
+BackendJavaScriptList[]=failover.js
+FrontendJavaScriptList[]=failover.js
 JavaScriptList[]=failover.js
 
 */ ?>
